c7672fdf12c188a6d2786688b5da170f781b314a
Matheus Alves <matheusalv.oliveira@gmail.com>
2024-04-08 16:50:16 -0300

    feedFood v2

--- src/components/FoodFeed/index.jsx
+++ src/components/FoodFeed/index.jsx
@@ -8,13 +8,13 @@ import './index.css';
 
 
 const Historic = () => {
-    
+
     const url = "http://localhost:3333/foods";
-    
+
     const [foods, setFoods] = useState([]);
-    
-     const getFoods = async () => {
-        
+
+    const getFoods = async () => {
+
         try {
             const response = await axios.get(url);
             const data = response.data;
@@ -26,37 +26,30 @@ const Historic = () => {
             alert("Não foi possivel carregador suas inserções, tente recarregar a página.");
         }
     };
-    
+
     useEffect(() => {
         getFoods();
     }, []);
 
     return (
-    <div>
-    <h1>Historico de rações</h1>
-
-        {foods.map((food) => (
-            <Container key={food.id} className='foodItem'>
-                    <Row>
-                        <Col>
-                            <Card style={{ width: '18rem' }}>
-                            <Card.Body>
+        <div>
+            <h1>Historico de rações</h1>
+            <Container className='foodItem'>
+                {foods.map((food) => (
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
                             <Card.Title>{food.brand}</Card.Title>
                             <Card.Subtitle className="mb-2 text-muted">{food.kg.$numberDecimal} Kg</Card.Subtitle>
                             <Card.Text>R$ {food.price.$numberDecimal},00</Card.Text>
                             <Card.Text>{food.date}</Card.Text>
                             <Card.Link href="#">Editar</Card.Link>
                             <Card.Link href="#">Deletar</Card.Link>
-                            </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
+                        </Card.Body>
+                    </Card>
+                ))}
             </Container>
-        )
-    )}
-    </div>    
-)
-        
+        </div>
+    )
 }
 
 export default Historic;
