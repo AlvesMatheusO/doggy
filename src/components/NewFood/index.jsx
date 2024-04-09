74fb93090a5d38ff76fbbd414099e7da4667323f
Matheus Alves <matheusalv.oliveira@gmail.com>
2024-04-09 10:45:59 -0300

    Send food request

--- src/components/NewFood/index.jsx
+++ src/components/NewFood/index.jsx
@@ -1,47 +1,46 @@
-import axios from 'axios';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import fetch from '../../axios/config.js'
 
-const InsertFood = async(e) => {
-    e.preventDefault();
+const InsertFood = () => {
 
     const [brand, setBrand] = useState();
     const [kg, setKg] = useState();
     const [price, setPrice] = useState();
 
-    const post = {brand, kg, price};
-    
-    await fetch.post("/food", {
-        body: post,
-    });
+    const createFood = async (e) => {
+        e.preventDefault();
+
+        await fetch.post("/food", {
+            brand: brand,
+            kg: kg,
+            price: price
+        });
+
+
+    }
+
 
     return (
         <div className='new-food'>
-            <Form>
-                
-                <Form.Group className="mb-3"  controlId="exampleForm.ControlTextarea1">
+            <Form onSubmit={createFood}>
+                <Form.Group className="mb-3" controlId="brand">
                     <Form.Label>Marca</Form.Label>
-                    <Form.Control name="marca" id='brand'  />
-                    <Form.Text className="text-muted">
-                    </Form.Text>
+                    <Form.Control name="brand" value={brand} onChange={(e) => setBrand(e.target.value)} />
+
                 </Form.Group>
 
-                <Form.Group className="mb-3"  controlId="exampleForm.ControlTextarea1"> 
+                <Form.Group className="mb-3" controlId="kg">
                     <Form.Label>Peso</Form.Label>
-                    <Form.Control name='peso' id='kg' />
-                    <Form.Text className="text-muted">
-                    </Form.Text>
+                    <Form.Control name="kg" value={kg} onChange={(e) => setKg(e.target.value)} />
                 </Form.Group>
 
-                <Form.Group className="mb-3"  controlId="exampleForm.ControlTextarea1">
+                <Form.Group className="mb-3" controlId="price">
                     <Form.Label>Preço</Form.Label>
-                    <Form.Control name='preco' id='price' />
-                    <Form.Text className="text-muted">
-                    </Form.Text>
+                    <Form.Control name="price" value={price} onChange={(e) => setPrice(e.target.value)} />
                 </Form.Group>
-                
+
                 <Button variant="primary" type="submit">Enviar</Button>
             </Form>
         </div>
