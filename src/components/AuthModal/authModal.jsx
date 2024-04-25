79f0b4dd4d1a4d760dbfc94cc94358e051217a44
AlvesMatheusO <matheusalv.oliveira@gmail.com>
2024-04-25 15:06:00 -0300

    registration working

--- src/components/AuthModal/authModal.jsx
+++ src/components/AuthModal/authModal.jsx
@@ -5,10 +5,31 @@ import './index.css';
 
 const Authentication = () => {
 
-    const [userName, setUserName] = useState('');
+    const [name, setname] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+
+    const register = async (e) => {
+        e.preventDefault();
+
+        try {
+            
+            await fetch.post("/auth/register", {
+                name: name,
+                email: email,
+                password: password,
+                confirmPassword: confirmPassword
+            });
+            console.log(name, email, password, confirmPassword);
+            console.log("usuário cadastrado!!!")
+
+        } catch (error) {
+            alert("Ocorreu um erro no seu cadastro. " + error);
+            console.log(error);
+            //console.log(name, email, password, confirmPassword);
+        }
+    }
 
     return (
         <div className="form-auth">
@@ -18,31 +39,31 @@ const Authentication = () => {
                 </div>
 
                 <div className="forms">
-                    <Form > {/* adicionar onSubmit  */}
+                    <Form onSubmit={register}>
                         <Form.Group className="mb-3" controlId="name">
                             <Form.Label>Nome</Form.Label>
-                            <Form.Control name="name" value={userName} /> {/* adicionar onChange  */}
+                            <Form.Control name="name" value={name} onChange={(e) => setname(e.target.value)}/> 
                         </Form.Group>
 
 
 
                         <Form.Group className="mb-3" controlId="email">
                             <Form.Label>E-mail</Form.Label>
-                            <Form.Control name="email" value={email} /> {/* adicionar onChange  */}
+                            <Form.Control name="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
                         </Form.Group>
 
 
 
                         <Form.Group className="mb-3" controlId="password">
                             <Form.Label>Senha</Form.Label>
-                            <Form.Control name="password" value={password} /> {/* adicionar onChange  */}
+                            <Form.Control name="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
                         </Form.Group>
 
 
 
                         <Form.Group className="mb-3" controlId="confirmPassword">
                             <Form.Label>Repita sua senha</Form.Label>
-                            <Form.Control name="confirmPassword" value={confirmPassword} /> {/* adicionar onChange  */}
+                            <Form.Control name="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
                         </Form.Group>
 
                         <button className='button' type="submit">Adicionar</button>
